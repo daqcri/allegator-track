bb6360abc7cf3832bc31f28703c495338df9e5f9
Hossam Hammady <github@hammady.net>
2014-06-01 13:59:37 +0300

    fixing bootstrap fonts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,2 @@
 Rails.application.config.assets.precompile += %w( main/index.css main/index.js main/algorithms.js jquery-ui-1.9.2/* )
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
