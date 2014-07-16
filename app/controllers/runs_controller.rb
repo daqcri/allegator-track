c6ca04c1d8756f2cc950afa2437d814a09a97699
szahbia <s.zahbia@gmai.com>
2014-07-16 11:30:32 +0300

    Confirming runset deletion

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -17,24 +17,44 @@ class RunsController < ApplicationController
   end
 
   def visualize
-    render json: @run.sankey
+      render json: @run.sankey
 
     # render json: {nodes:[
-    #   {name: "A"},
-    #   {name: "B"},
-    #   {name: "C"},
-    #   {name: "D"},
-    #   {name: "E"},
-    #   {name: "F"},
-    #   {name: "G"},
+    #   {name: "Best Bargain Books"},
+    #   {name: "textbookxdotcom"},
+    #   {name: "OPOE-ABE Books"},
+    #   {name: "Player Quest"},
+    #   {name: "Bobs Books"},
+    #   {name: "Mellon's Books"},
+    #   {name: "Blackwell Online"},
+    #   {name: "1"},
+    #   {name: "2"},
+    #   {name: "3"},
+    #   {name: "4"},
+    #   {name: "True"},
+    #   {name: "False"}
     #   ],
     #   links:[
-    #   { source:0, target:1, value:2},
-    #   { source:2, target:3, value:20},
-    #   { source:2, target:1, value:5}, 
-    #   { source:4, target:2, value:5},
-    #   { source:4, target:5, value:5},
-    #   { source:5, target:6, value:5}
+    #   { source:0, target:7, value:2},
+    #   { source:7, target:11, value:3},
+    #   { source:1, target:10, value:6}, 
+    #   { source:10, target:12, value:5},
+    #   { source:1, target:9, value:5},
+    #   { source:2, target:9, value:3},
+    #   { source:9, target:11, value:1},
+    #   { source:2, target:10, value:8},
+    #   { source:3, target:8, value:4},
+    #   { source:8, target:11, value:6},
+    #   { source:4, target:8, value:15},
+    #   { source:8, target:12, value:4},
+    #   { source:5, target:7, value:10},
+    #   { source:9, target:12, value:4},
+    #   { source:6, target:8, value:4},
+    #   { source:8, target:12, value:6},
+    #   { source:6, target:10, value:9},
+    #   { source:10, target:11, value:1},
+    #   { source:6, target:7, value:9},
+    #   { source:7, target:11, value:1}
     # ]}
   end
 
