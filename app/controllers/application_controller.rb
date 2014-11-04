34d4e1aaf48391c01c41338190f961ccfa8032e2
goelnaman <goelnaman2@gmail.com>
2014-11-04 16:12:27 +0300

    length parameter optional in viewing sets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  #protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
   def limit_query(query)
     start = params[:start].to_i
-    length = params[:length].to_i
+    length = length=ENV['DEFAULT_DATATABLE_LENGTH'].to_i
+    length = params[:length].to_i if params[:length].present?
     query = query.offset(start)
     query = query.limit(length) if length > 0
   end
