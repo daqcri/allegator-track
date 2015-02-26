b013da1705f6af6cdeb1a23c4c2fa5f299513b66
Hossam Hammady <github@hammady.net>
2015-02-26 16:22:45 +0300

    Hiding runs table by default, making it always flying from bottom, restructuring the whole layout of the page to be simpler, closes #100, closes #107

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,3 @@
 Rails.application.config.assets.precompile += %w( main/index.css main/index.js main/algorithms.js main/visualize.js main/hash_with_length.js jquery-ui-1.9.2/* )
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
-Rails.application.config.assets.precompile += %w(sort_both.png sort_asc.png sort_desc.png loading-background.png indicator.gif)
+Rails.application.config.assets.precompile += %w(sort_both.png sort_asc.png sort_desc.png loading-background.png indicator.gif go-up-on.gif go-lt-off.gif go-rt-off.gif)
