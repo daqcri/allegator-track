0121d1d0b893267d529cabf03449e6456a1028c8
Hossam Hammady <github@hammady.net>
2016-04-03 10:34:36 +0300

    overriding host to 0.0.0.0

--- run.py
+++ run.py
@@ -6,8 +6,8 @@ import os, argparse
 redisSeed = RedisTokenSeed()
 redisSeed.putTokens()
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-p','--port', help='PORT', required=True)
     args = vars(parser.parse_args())
-    app.run(port = int(os.environ.get("PORT", None) or args['port']))
+    app.run(host='0.0.0.0', port = int(os.environ.get("PORT", None) or args['port']))
