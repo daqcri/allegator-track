05d18c62fc681afac0e11eec773b53037c9a7842
Kushal <kns971@gmail.com>
2016-04-03 10:09:23 +0300

    Arg-Parse

--- run.py
+++ run.py
@@ -2,9 +2,12 @@
 
 from app import app
 from redisTokenSeeder import RedisTokenSeed
-import os
+import os, argparse
 redisSeed = RedisTokenSeed()
 redisSeed.putTokens()
 
 if __name__ == '__main__': 
-    app.run(port = int(os.environ.get("PORT", None)))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p','--port', help='PORT', required=True)
+    args = vars(parser.parse_args())
+    app.run(port = int(os.environ.get("PORT", None) or args['port']))
